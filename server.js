8f80fe7d8b6bcb236d918cc2cc843339f2ece306
MNaguib2611 <m.naguib2611@gmail.com>
2020-04-03 23:53:20 +0200

    finished-event-source

--- server.js
+++ server.js
@@ -1,23 +1,43 @@
+const express=require('express');
+const bodyParser = require('body-parser')
 const cors =require('cors'); 
-const app=require('express')();
-const socketIo =require('socket.io'); 
-const server =require('http').Server(app); 
 
 
-
+const app = express();
+app.use(express.json());
 app.use (cors());
 
 
-const io = socketIo(server);
+const subscribers ={};
 
-io.on('connection',(client)=>{
-    client.on('message',(message)=>{
-        io.emit('new-message',message);
-    });
+
+app.get("/subscribe",(req,res) =>{
+    let  id= Math.ceil(Math.random() * 10000);
+    console.log('\x1b[32m%s\x1b[0m',`user  ${id} is now subscribed`);
+    const header = {
+        "Content-Type":"text/event-stream",
+        "Cache-Control":"no-cache",
+        Connection:"keep-alive",
+    }
+    
+    res.writeHead(200,header);
+    subscribers[id] = res;
+    
+})
+
+app.post("/messageSubscribers",(req,res) =>{
+    const { body }=req;
+    console.log(subscribers);
+    // console.log(body);
+   Object.keys(subscribers).forEach((subID)=> {
+    subscribers[subID].write(`data: ${JSON.stringify(body)}\n\n`);
+   });
+   res.status(204).end();
 })
 
 
-server .listen(3000,(err)=>{
+
+const server = app.listen(3000,(err)=>{
     if (!err) console.log('\x1b[32m%s\x1b[0m','Server was started on port 3000');
 });
 
